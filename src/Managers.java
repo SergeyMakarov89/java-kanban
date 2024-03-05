f435b730b8875d6bd97703771475f9e2ec0f6afa
Sergey Makarov <spek94@yandex.ru>
2024-03-05 22:35:23 +0500

    Поправил codestyle после тестов с пулл реквеста

--- src/Managers.java
+++ src/Managers.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Managers {
     public static TaskManager makeInMemoryTaskManager() {
         TaskManager inMemoryTaskManager = new InMemoryTaskManager();
